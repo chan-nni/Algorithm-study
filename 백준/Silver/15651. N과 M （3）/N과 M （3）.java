c8438c8a642e61cb7e0a80e7b0e037e6b8558eac
chan-nni <85616993+chan-nni@users.noreply.github.com>
2025-02-02 01:55:46 +0900

    [Silver III] Title: N과 M (3), Time: 380 ms, Memory: 67844 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, R;
+    static int[] nums;
+    static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        nums = new int[R];
+        perm(0);
+        System.out.print(result);
+    }
+
+    private static void perm(int cnt) {
+        if (cnt == R) {
+            for (int i = 0; i < R; i++) {
+                result.append(nums[i]).append(" ");
+            }
+            result.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            nums[cnt] = i + 1;
+            perm(cnt + 1);
+        }
+    }
+}
