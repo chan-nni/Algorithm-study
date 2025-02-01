c3d906945fcb44547504f21f856b32f0b566730a
chan-nni <85616993+chan-nni@users.noreply.github.com>
2025-02-02 01:56:23 +0900

    [Silver III] Title: N과 M (5), Time: 296 ms, Memory: 28488 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] input;
+    static int[] nums;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        input = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(input);
+
+        nums = new int[M];
+        visited = new boolean[N];
+
+        perm(0);
+        System.out.print(sb);
+    }
+
+    private static void perm(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(nums[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                nums[depth] = input[i];
+                perm(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
